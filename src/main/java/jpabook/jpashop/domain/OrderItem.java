b8db9534297266ac86263208666b0a26d9bc8d91
offMomySon <ohji100629@naver.com>
2020-12-18 19:11:27 +0900

    develope domain

---
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -0,0 +1,28 @@
+package jpabook.jpashop.domain;
+
+import jpabook.jpashop.domain.item.Item;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+import static javax.persistence.FetchType.LAZY;
+
+@Entity
+@Getter @Setter
+public class OrderItem {
+    @Id @GeneratedValue
+    @Column(name="order_item_id")
+    private Long id;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name="item_id")
+    private Item item;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name="order_id")
+    private Order order;
+
+    private int orderPrice; // 주문 가격  -> 상품가격이 나중에 변경될 수 있다.
+    private int count;      // 주문 수량  -> 주문 당시의 수량
+}
